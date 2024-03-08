a110878544dda6c3955e614adcbecb4e82d32701
Shubham Sah <42349247+shubhamsah@users.noreply.github.com>
2024-03-08 17:00:05 +0530

    Added footer

--- app.py
+++ app.py
@@ -128,9 +128,14 @@ def generative_config():
 
     return config
 
+def custom_footer():
+    footer = ''' Made with <svg viewBox="0 0 1792 1792" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" style="height: 0.8rem;"><path d="M896 1664q-26 0-44-18l-624-602q-10-8-27.5-26T145 952.5 77 855 23.5 734 0 596q0-220 127-344t351-124q62 0 126.5 21.5t120 58T820 276t76 68q36-36 76-68t95.5-68.5 120-58T1314 128q224 0 351 124t127 344q0 221-229 450l-623 600q-18 18-44 18z" fill="#e25555"></path></svg> by Shubham Sah'''
+    st.markdown(footer, unsafe_allow_html=True)
+
 def main():
     #Initialize Streamlit
     initialize_streamlit()
+    custom_footer()
 
     # Input text area
     user_input_text = user_input()
@@ -155,6 +160,7 @@ def main():
         if response is not None:
             response_placeholder.subheader("The Response is")
             response_placeholder.write(response)
+        
 
 if __name__ == "__main__":
     main()
